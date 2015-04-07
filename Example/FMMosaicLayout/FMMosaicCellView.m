e266c8ccf5ca72b2dd48dd138670c7fcfb93ee63
JVillella <julian.villella@hotmail.com>
2015-04-07 15:33:56 -0400

    Adds spring animation on cell tap

--- Example/FMMosaicLayout/FMMosaicCellView.m
+++ Example/FMMosaicLayout/FMMosaicCellView.m
@@ -27,10 +27,44 @@
 
 static NSString* const kFMMosaicCellViewReuseIdentifier = @"FMMosaicCellViewReuseIdentifier";
 
+@interface FMMosaicCellView () <UIGestureRecognizerDelegate>
+
+@property (weak, nonatomic) IBOutlet UIView *overlayView;
+
+@end
+
 @implementation FMMosaicCellView
 
 + (NSString *)reuseIdentifier {
     return kFMMosaicCellViewReuseIdentifier;
+}
+
+- (void)awakeFromNib {
+    UILongPressGestureRecognizer *longPressGesture = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(onLongPress:)];
+    longPressGesture.minimumPressDuration = 0.0;
+    longPressGesture.allowableMovement = self.frame.size.width / 2.0; // Approximation
+    longPressGesture.delegate = self;
+    longPressGesture.cancelsTouchesInView = NO;
+    [self addGestureRecognizer:longPressGesture];
+}
+
+- (void)onLongPress:(UILongPressGestureRecognizer *)longPressGestureRecognizer {
+    if (longPressGestureRecognizer.state == UIGestureRecognizerStateBegan) {
+        [UIView animateWithDuration:0.2 delay:0.0 usingSpringWithDamping:0.6 initialSpringVelocity:0.8 options:UIViewAnimationOptionCurveEaseOut animations:^{
+            self.transform = CGAffineTransformMakeScale(0.9, 0.9);
+            self.overlayView.alpha = 0.35;
+        } completion:nil];
+        
+    } else {
+        [UIView animateWithDuration:0.2 delay:0.0 usingSpringWithDamping:0.6 initialSpringVelocity:0.8 options:UIViewAnimationOptionCurveEaseOut animations:^{
+            self.transform = CGAffineTransformIdentity;
+            self.overlayView.alpha = 0.0;
+        } completion:nil];
+    }
+}
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
+    return YES;
 }
 
 @end
